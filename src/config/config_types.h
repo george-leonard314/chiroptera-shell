b000dad834944e661206d7416a2384af47746124
Alexander Nabokikh <alexander.nabokikh@olx.pl>
2026-06-11 10:11:50 +0200

    feat(launcher): added seassion search option

--- src/config/config_types.h
+++ src/config/config_types.h
@@ -746,6 +746,7 @@ struct ShellConfig {
     bool launcherCategories = true;
     bool launcherShowIcons = true;
     bool launcherCompact = false;
+    bool launcherSessionSearch = false;
 
     bool operator==(const PanelConfig&) const = default;
   };
