c13338da540ebc0d06808314d1cf63d3bf7b549c
Lemmy <studio@quadbyte.net>
2026-06-11 17:50:59 -0400

    render(label): add support for start and middle eliding

--- src/render/render_context.cpp
+++ src/render/render_context.cpp
@@ -222,9 +222,9 @@ void RenderContext::renderScene(RenderTarget& target, Node* sceneRoot) {
 
 TextMetrics RenderContext::measureText(
     std::string_view text, float fontSize, FontWeight fontWeight, float maxWidth, int maxLines, TextAlign align,
-    std::string_view fontFamily
+    std::string_view fontFamily, TextEllipsize ellipsize
 ) {
-  auto m = m_textRenderer.measure(text, fontSize, fontWeight, maxWidth, maxLines, align, fontFamily);
+  auto m = m_textRenderer.measure(text, fontSize, fontWeight, maxWidth, maxLines, align, fontFamily, ellipsize);
   return TextMetrics{
       .width = m.width,
       .left = m.left,
@@ -344,14 +344,14 @@ void RenderContext::renderNode(
         const Mat3 shadowTransform = worldTransform * Mat3::translation(text->shadowOffsetX(), text->shadowOffsetY());
         m_textRenderer.draw(
             sw, sh, 0.0f, 0.0f, text->text(), text->fontSize(), shadowColor, shadowTransform, text->fontWeight(),
-            text->maxWidth(), text->maxLines(), text->textAlign(), font
+            text->maxWidth(), text->maxLines(), text->textAlign(), font, text->ellipsize()
         );
       }
       auto color = text->color();
       color.a *= effectiveOpacity;
       m_textRenderer.draw(
           sw, sh, 0.0f, 0.0f, text->text(), text->fontSize(), color, worldTransform, text->fontWeight(),
-          text->maxWidth(), text->maxLines(), text->textAlign(), font
+          text->maxWidth(), text->maxLines(), text->textAlign(), font, text->ellipsize()
       );
     }
     break;
