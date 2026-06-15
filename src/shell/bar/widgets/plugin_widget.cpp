b8a387f6a3ee4ab000456e3f8e33ed8cce287bce
Lemmy <studio@quadbyte.net>
2026-06-14 21:46:33 -0400

    fix(plugin-ipc): pass focused output snapshot to services

--- src/shell/bar/widgets/plugin_widget.cpp
+++ src/shell/bar/widgets/plugin_widget.cpp
@@ -498,7 +498,9 @@ void PluginWidget::luaSetVisible(bool visible) {
   m_dirty = true;
 }
 
-PluginWidget::DispatchResult PluginWidget::dispatchIpc(std::string_view event, std::string_view payload) {
+PluginWidget::DispatchResult
+PluginWidget::dispatchIpc(std::string_view event, std::string_view payload, const scripting::ScriptSnapshot& snapshot) {
+  (void)snapshot;
   if (!m_runtime) {
     return DispatchResult::MissingHost;
   }
